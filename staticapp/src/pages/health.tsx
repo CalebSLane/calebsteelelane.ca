6ed6a8600212db4e7e6858c62d06b4d25f767872
CalebSLane <c.steelelane@gmail.com>
2025-06-22 14:32:06 -0700

    add health page to staticapp

---
+++ staticapp/src/pages/health.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { HeadFC, PageProps } from 'gatsby';
+import 'purecss/build/pure-min.css';
+import 'purecss/build/grids-responsive.css';
+import '../css/styles.css';
+
+const HealthPage: React.FC<PageProps> = ({ data }) => {
+  return <main></main>;
+};
+
+export default HealthPage;
+
+export const Head: HeadFC = () => <title>health</title>;
