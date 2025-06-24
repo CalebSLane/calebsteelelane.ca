4333e7587ceff086c50f793a28d657e0105da40e
CalebSLane <c.steelelane@gmail.com>
2025-06-24 15:03:53 -0700

    generate graphql typegen on build

--- staticapp/gatsby-config.ts
+++ staticapp/gatsby-config.ts
@@ -8,10 +8,11 @@ const config: GatsbyConfig = {
     // author: `@CalebSteele-Lane`,
     keywords: `developer, software engineer, security consultant, web developer, full-stack developer, javascript, java, typescript, gatsby`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  graphqlTypegen: {
+    typesOutputPath: `gatsby-types.d.ts`,
+    generateOnBuild: true,
+    documentSearchPaths: [`./gatsby-node.ts`, `./plugins/**/gatsby-node.ts`],
+  },
   flags: {
     DEV_SSR: true,
   },
